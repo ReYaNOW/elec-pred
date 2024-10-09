85be80fca270c63e8e2157ddc5243845426c487b
Sergei <reyangood@gmail.com>
2024-10-10 02:32:53 +0300

    add localhost to ALLOWED_ORIGINS

--- elec_pred/settings.py
+++ elec_pred/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config.secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.debug
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
 
 
 # Application definition
